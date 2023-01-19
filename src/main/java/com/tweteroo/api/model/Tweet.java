3162b6cfd1ac4ae165535670e4a3c63b1a9d80f6
Alice Amorim <alice.amorim@engenharia.ufjf.br>
2023-01-19 02:05:12 -0300

    create Tweet model

--- src/main/java/com/tweteroo/api/model/Tweet.java
+++ src/main/java/com/tweteroo/api/model/Tweet.java
@@ -1,5 +1,7 @@
 package com.tweteroo.api.model;
 
+import com.tweteroo.api.dto.TweetDTO;
+
 import jakarta.persistence.*;
 import lombok.Data;
 
@@ -7,7 +9,23 @@ import lombok.Data;
 @Entity
 @Table(name = "tweets")
 public class Tweet {
+
+    public Tweet (TweetDTO data) {
+        this.username = data.username();
+        this.avatar = data.avatar();
+        this.text = data.text();
+    }
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @Column(length = 20, nullable = false)
+    private String username;
+
+    @Column(length = 200, nullable = false)
+    private String avatar;
+
+    @Column(length = 140, nullable = false)
+    private String text;
 }
