2645ab5d0465ddab01543a9ab353758812b74095
Alice Amorim <alice.amorim@engenharia.ufjf.br>
2023-01-19 02:00:41 -0300

    create User model

--- src/main/java/com/tweteroo/api/model/User.java
+++ src/main/java/com/tweteroo/api/model/User.java
@@ -10,4 +10,18 @@ import lombok.Data;
 @Table(name = "users")
 public class User {
 
+    public User(UserDTO data) {
+        this.username = data.username();
+        this.avatar = data.avatar();
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(length = 20, nullable = false)
+    private String username;
+
+    @Column(length = 200, nullable = false)
+    private String avatar;
 }
