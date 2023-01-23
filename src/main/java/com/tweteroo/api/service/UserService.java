69a98f141ce5ecc3d2aa2adb95d88a29ae2b41c0
Alice Amorim <alice.amorim@engenharia.ufjf.br>
2023-01-23 15:24:56 -0300

    Create UserService

---
+++ src/main/java/com/tweteroo/api/service/UserService.java
@@ -0,0 +1,16 @@
+package com.tweteroo.api.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.tweteroo.api.repository.UserRepository;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+
+
+}
